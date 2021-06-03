043ad6b9e0707e2eaf377232e39e4ede86a6c6ce
svenster18 <mohamadrizki8@gmail.com>
2021-06-03 09:30:36 +0700

    Membuat validasi pada RegistrationActivity.kt dan memperbaiki error pada RecordCoughActivity.kt

--- app/src/main/java/com/gachateam/covtect_19/RecordCoughActivity.kt
+++ app/src/main/java/com/gachateam/covtect_19/RecordCoughActivity.kt
@@ -2,123 +2,174 @@ package com.gachateam.covtect_19
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.media.AudioFormat
-import android.media.MediaPlayer
-import android.media.MediaRecorder
+import android.media.*
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
+import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.gachateam.covtect_19.databinding.ActivityRecordCoughBinding
 import java.io.File
-import java.io.FileOutputStream
 import java.io.IOException
 
-class RecordCoughActivity : AppCompatActivity() {
+class RecordCoughActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val EXTRA_USER = "extra_user"
-        const val fileName = "/user.wav"
+        const val RECORD_PERMISSION = Manifest.permission.RECORD_AUDIO
+        const val WRITE_PERMISSION = Manifest.permission.WRITE_EXTERNAL_STORAGE
+        const val READ_PERMISSION = Manifest.permission.READ_EXTERNAL_STORAGE
+        const val RECORD_PERMISSION_CODE = 21;
+        const val WRITE_PERMISSION_CODE = 31;
+        const val READ_PERMISSION_CODE = 41;
     }
 
     private lateinit var binding: ActivityRecordCoughBinding
-    private lateinit var mediaRecorder: MediaRecorder
-    private val file = Environment.getExternalStorageDirectory().absolutePath + fileName
-    private val fileOutput = File(file)
-    private lateinit var mediaPlayer: MediaPlayer
+    private var mediaRecorder: MediaRecorder? = null
+    private lateinit var fileName: String
+    private lateinit var filePath: String
+    private var isRecord = false
+    private var mMediaPlayer: MediaPlayer? = null
+    private var isReady: Boolean = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecordCoughBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val user = intent.getParcelableExtra<User>(EXTRA_USER)
+        val user = intent.getParcelableExtra<User>(EXTRA_USER)!!
 
-        mediaRecorder = MediaRecorder()
-        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-        mediaRecorder.setOutputFormat(AudioFormat.ENCODING_PCM_16BIT)
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-        mediaRecorder.setAudioChannels(1)
-        mediaRecorder.setAudioEncodingBitRate(128000)
-        mediaRecorder.setAudioSamplingRate(48000)
-        fileOutput.createNewFile()
-        mediaRecorder.setOutputFile(fileOutput.toString())
+        filePath = this.getExternalFilesDir("/")?.absolutePath.toString()
+        fileName = "${user?.userId}.wav"
+        binding.ibRecord.setOnClickListener(this)
 
-        var isRecord = false
+        binding.ibPlay.setOnClickListener (this)
+    }
 
-        binding.ibRecord.setOnClickListener {
-            isRecord = !isRecord
-            if(isRecord) {
-                startRecord()
-                Toast.makeText(this, "Recording", Toast.LENGTH_SHORT).show()
-                binding.ibRecord.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_stop_24))
-            } else {
-                // stop
-                stopRecord()
-                Toast.makeText(this, "Stop Recording", Toast.LENGTH_SHORT).show()
-                binding.ibRecord.setImageDrawable(resources.getDrawable(R.drawable.ic_mic_white_24))
+    override fun onClick(v: View) {
+        when (v.id) {
+            binding.ibRecord.id -> {
+                if(isRecord) {
+                    // stop Record
+                    stopRecord()
+                    Toast.makeText(this, "Stop Recording", Toast.LENGTH_SHORT).show()
+                    binding.ibRecord.setImageDrawable(resources.getDrawable(R.drawable.ic_mic_white_24))
+                    isRecord = false
+                } else {
+                    if (checkPermissions()) {
+                        startRecord()
+                        Toast.makeText(this, "Recording", Toast.LENGTH_SHORT).show()
+                        binding.ibRecord.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_stop_24))
+                        isRecord = true
+                    }
+                }
             }
-        }
+            binding.ibPlay.id -> {
+                playAndStop()
+            }
+            binding.btnKirim.id -> {
 
-        mediaPlayer = MediaPlayer()
-
-
-        var isPlay = false
-
-        binding.ibPlay.setOnClickListener {
-            isPlay = !isPlay
-            if(isPlay) {
-                // play
-                Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
-                binding.ibPlay.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_stop_24))
-                play()
-            } else {
-                // stop
-                stop()
-                Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show()
-                binding.ibPlay.setImageDrawable(resources.getDrawable(R.drawable.ic_play_arrow_white_24))
             }
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (fileOutput.exists()) {
-            fileOutput.delete()
+    private fun checkPermissions(): Boolean {
+        var isRecordGranted = false
+        var isWriteGranted = false
+        var isReadGranted = false
+        if (ActivityCompat.checkSelfPermission(this, RECORD_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            isRecordGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(RECORD_PERMISSION), RECORD_PERMISSION_CODE)
+            isRecordGranted = false
         }
+        if (ActivityCompat.checkSelfPermission(this, WRITE_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            isWriteGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(WRITE_PERMISSION), WRITE_PERMISSION_CODE)
+            isWriteGranted = false
+        }
+        if (ActivityCompat.checkSelfPermission(this, READ_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            isReadGranted = true
+        } else {
+            ActivityCompat.requestPermissions(this, arrayOf(READ_PERMISSION), READ_PERMISSION_CODE)
+            isReadGranted = false
+        }
+        return isRecordGranted && isWriteGranted && isReadGranted
     }
-
     private fun startRecord() {
+        val file = File(filePath + "/" + fileName)
+        if (file.exists()) {
+            file.delete()
+        }
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(AudioFormat.ENCODING_PCM_16BIT)
+        mediaRecorder?.setOutputFile(filePath + "/" + fileName)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        mediaRecorder?.setAudioChannels(1)
+        mediaRecorder?.setAudioEncodingBitRate(128000)
+        mediaRecorder?.setAudioSamplingRate(48000)
+
         try {
-            if (fileOutput.exists()) {
-                fileOutput.delete()
-            }
-            mediaRecorder.setOutputFile(fileOutput.toString())
-            mediaRecorder.prepare()
-            mediaRecorder.start()
+            mediaRecorder?.prepare()
         } catch (e: IOException) {
             e.printStackTrace()
         }
+        mediaRecorder?.start()
     }
 
     private fun stopRecord() {
-        mediaRecorder.stop()
-        mediaRecorder.release()
+        mediaRecorder?.stop()
+        mediaRecorder?.release()
+        mediaRecorder = null
     }
 
-    private fun play() {
+    private fun playAndStop() {
+        init()
         try {
-            mediaPlayer.setDataSource(fileOutput.toString())
-            mediaPlayer.prepare()
-            mediaPlayer.start()
+            //mediaPlayer.setDataSource()
+            if (!isReady) {
+                mMediaPlayer?.prepareAsync()
+            } else {
+                if (mMediaPlayer?.isPlaying() as Boolean || isReady) {
+                    mMediaPlayer?.stop()
+                    Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show()
+                    binding.ibPlay.setImageDrawable(resources.getDrawable(R.drawable.ic_play_arrow_white_24))
+                    isReady = false
+                }else {
+                    Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show()
+                    binding.ibPlay.setImageDrawable(resources.getDrawable(R.drawable.ic_baseline_stop_24))
+                    mMediaPlayer?.start()
+                }
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
     }
 
-    private fun stop() {
-        mediaPlayer.stop()
-        mediaPlayer.release()
+    private fun init() {
+        mMediaPlayer = MediaPlayer()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val attribute = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .build()
+            mMediaPlayer?.setAudioAttributes(attribute)
+        } else {
+            mMediaPlayer?.setAudioStreamType(AudioManager.STREAM_MUSIC)
+        }
+        try {
+            mMediaPlayer?.setDataSource(filePath + "/" + fileName)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        mMediaPlayer?.setOnPreparedListener({
+            isReady = true
+            mMediaPlayer?.start()
+        })
+        mMediaPlayer?.setOnErrorListener({ mp, what, extra -> false })
     }
 }
--- app/src/main/java/com/gachateam/covtect_19/RegistrationActivity.kt
+++ app/src/main/java/com/gachateam/covtect_19/RegistrationActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gachateam.covtect_19.databinding.ActivityRegistrationBinding
+import java.lang.NullPointerException
+import java.lang.NumberFormatException
 
 class RegistrationActivity : AppCompatActivity() {
 
@@ -20,9 +22,11 @@ class RegistrationActivity : AppCompatActivity() {
 
             val user = registration()
 
-            val intent = Intent(this@RegistrationActivity, SymptomActivity::class.java)
-            intent.putExtra(SymptomActivity.EXTRA_USER, user)
-            startActivity(intent)
+            if (user != null) {
+                val intent = Intent(this@RegistrationActivity, SymptomActivity::class.java)
+                intent.putExtra(SymptomActivity.EXTRA_USER, user)
+                startActivity(intent)
+            }
         }
 
         binding.btnCancel.setOnClickListener {
@@ -30,16 +34,39 @@ class RegistrationActivity : AppCompatActivity() {
         }
     }
 
-    private fun registration(): User {
-        val age = binding.edtAge.text.toString().toInt()
+    private fun registration(): User? {
+        var age = 0
+        var height = 0
+        var weight = 0
+
+        try {
+            age = binding.edtAge.text.toString().toInt()
+        } catch (e: NullPointerException) {
+            binding.edtAge.error = "Wajib Diisi"
+        } catch (e: NumberFormatException) {
+            binding.edtAge.error = "Hanya diperbolehkan memasukkan Angka"
+            return null
+        }
+
         val gender = if (binding.rbMale.isChecked) {
             "Laki-Laki"
         } else {
             "Perempuan"
         }
-        val height = binding.edtHeight.text.toString().toInt()
-        val weight = binding.edtWeight.text.toString().toInt()
 
-        return User(null, age, gender, height, weight)
+        try {
+            height = binding.edtHeight.text.toString().toInt()
+        } catch (e: NumberFormatException) {
+            binding.edtHeight.error = "Hanya diperbolehkan memasukkan Angka"
+            return null
+        }
+
+        try {
+            weight = binding.edtWeight.text.toString().toInt()
+            return User(null, age, gender, height, weight)
+        } catch (e: NumberFormatException) {
+            binding.edtWeight.error = "Hanya diperbolehkan memasukkan Angka"
+            return null
+        }
     }
 }
